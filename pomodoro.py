063a1969583a0062d240672e193bbb4536a786f2
Levi Henley <levicolemagic@gmail.com>
2023-08-15 02:58:00 -0700

    chore: Refactor set_up_menu and start_timer methods
    This commit refactors the set_up_menu method in the PomodoroApp class to update the menu with new buttons and options based on the configuration. It also refactors the start_timer method to properly set the timer and update the start/pause button text.

--- pomodoro.py
+++ pomodoro.py
@@ -26,7 +26,23 @@ class PomodoroApp(object):
         self.timer = rumps.Timer(self.on_tick, 1)
         self.set_up_menu()
 
-   
+    def set_up_menu(self):
+        if hasattr(self, 'timer') and self.timer is not None:
+            self.timer.stop()
+
+        self.app.menu.clear()
+
+        self.start_pause_button = rumps.MenuItem(title=self.config["start"], callback=self.start_timer)
+        self.stop_button = rumps.MenuItem(title=self.config["stop"], callback=self.stop_timer)
+        self.toggle_transition_button = rumps.MenuItem(title="Auto Transition: ON", callback=self.toggle_transition)
+        self.stats_button = rumps.MenuItem(title="Show Statistics", callback=self.show_statistics)
+        self.skip_button = rumps.MenuItem(title="Skip", callback=self.skip_phase)
+        self.settings_button = rumps.MenuItem(title="Settings", callback=self.get_user_input)
+        
+        self.app.menu = [self.start_pause_button, self.stop_button, self.toggle_transition_button, self.stats_button, self.skip_button, self.settings_button]
+        
+        self.mode = "pomodoro"
+        self.auto_transition = True
 
     def show_statistics(self, _):
         stats_message = f"Completed Pomodoros: {self.statistics['completed_pomodoros']}\n" \
@@ -35,8 +51,7 @@ class PomodoroApp(object):
         rumps.alert("Statistics", stats_message)
 
     def log_task(self):
-        task_window = rumps.Window(message='What are you working on?')
-        task_window.default_text = ""
+        task_window = rumps.Window(message='What are you working on?', default_input="")
         task_response = task_window.run()
         if task_response.clicked:
             self.tasks_log.append(task_response.text)
@@ -53,34 +68,31 @@ class PomodoroApp(object):
 
     
     def start_timer(self, sender):
-        if sender.title.lower().startswith(("start", "continue")):
-            if sender.title == self.config["start"]:
-                self.timer.count = 0
-                if self.mode == "pomodoro":
-                    self.timer.end = self.config["pomodoro_interval"]
-                elif self.mode == "break":
-                    if self.pomodoro_counter % 4 == 0:
-                        self.timer.end = self.config["long_break_interval"]
-                    else:
-                        self.timer.end = self.config["short_break_interval"]
-                self.log_task()
-            sender.title = self.config["pause"]
-            self.timer.start()
-        else:
-            sender.title = self.config["continue"]
-            self.timer.stop()
+        if self.mode == "pomodoro":
+            self.log_task()
+        self.app = rumps.App(self.config["app_name"])
+        self.timer = rumps.Timer(self.on_tick, 1)
 
-    def get_user_input(self, _):
-        pomodoro_window = rumps.Window(message='Set Pomodoro Duration (in minutes):')
-        pomodoro_window.default_text = str(self.config["pomodoro_interval"] // 60)
+        self.settings_button = rumps.MenuItem(title="Settings", callback=self.get_user_input)
+        self.app.menu.add(self.settings_button)
+        
+        self.interval = self.config["pomodoro_interval"]
+        
+        self.start_pause_button = rumps.MenuItem(title=self.config["start"], callback=self.start_timer)
+        self.stop_button = rumps.MenuItem(title=self.config["stop"], callback=self.stop_timer)
+        self.toggle_transition_button = rumps.MenuItem(title="Auto Transition: ON", callback=self.toggle_transition)
+        
+        self.app.menu = [self.start_pause_button, self.stop_button, self.toggle_transition_button]
+        self.set_up_menu()
+
+    def get_user_input(self):
+        pomodoro_window = rumps.Window(message='Set Pomodoro Duration (in minutes):', default_input=str(self.config["pomodoro_interval"] // 60))
         pomodoro_response = pomodoro_window.run()
 
-        short_break_window = rumps.Window(message='Set Short Break Duration (in minutes):')
-        short_break_window.default_text = str(self.config["short_break_interval"] // 60)
+        short_break_window = rumps.Window(message='Set Short Break Duration (in minutes):', default_input=str(self.config["short_break_interval"] // 60))
         short_break_response = short_break_window.run()
 
-        long_break_window = rumps.Window(message='Set Long Break Duration (in minutes):')
-        long_break_window.default_text = str(self.config["long_break_interval"] // 60)
+        long_break_window = rumps.Window(message='Set Long Break Duration (in minutes):', default_input=str(self.config["long_break_interval"] // 60))
         long_break_response = long_break_window.run()
 
         if pomodoro_response.clicked and short_break_response.clicked and long_break_response.clicked:
@@ -91,34 +103,38 @@ class PomodoroApp(object):
             except ValueError:
                 rumps.alert("Please enter valid numbers!")
     def set_up_menu(self):
-        if hasattr(self, 'timer') and self.timer is not None:
-            self.timer.stop()
-            self.timer.count = 0
-            
+        self.timer.stop()
+        self.timer.count = 0
         self.app.title = "🍅"
         self.pomodoro_counter = 0
         self.mode = "pomodoro"
         self.auto_transition = True
-        self.app.menu.clear()
 
-        self.start_pause_button = rumps.MenuItem(title=self.config["start"], callback=self.start_timer)
-        self.stop_button = rumps.MenuItem(title=self.config["stop"], callback=self.stop_timer)
-        self.toggle_transition_button = rumps.MenuItem(title="Auto Transition: ON", callback=self.toggle_transition)
-        self.stats_button = rumps.MenuItem(title="Show Statistics", callback=self.show_statistics)
-        self.skip_button = rumps.MenuItem(title="Skip", callback=self.skip_phase)
-        self.settings_button = rumps.MenuItem(title="Settings", callback=self.get_user_input)
-        
-        self.app.menu = [self.start_pause_button, self.stop_button, self.toggle_transition_button, self.stats_button, self.skip_button, self.settings_button]
-    
+    def start_timer(self, sender):
+        if sender.title.lower().startswith(("start", "continue")):
+            if sender.title == self.config["start"]:
+                self.timer.count = 0
+                if self.mode == "pomodoro":
+                    self.timer.end = self.config["pomodoro_interval"]
+                elif self.mode == "break":
+                    if self.pomodoro_counter % 4 == 0:
+                        self.timer.end = self.config["long_break_interval"]
+                    else:
+                        self.timer.end = self.config["short_break_interval"]
+            sender.title = self.config["pause"]
+            self.timer.start()
+        else:
+            sender.title = self.config["continue"]
+            self.timer.stop()
+
     def on_tick(self, sender):
         time_left = sender.end - sender.count
-        mins = time_left // 60 if time_left >= 0 else time_left // 60 + 1
-        secs = time_left % 60 if time_left >= 0 else (-1 * time_left) % 60
-
+        # Add the progress bar code here
         progress_percentage = (sender.count / sender.end)
         progress_blocks = int(progress_percentage * 10)
         progress_bar = '▓' * progress_blocks + '░' * (10 - progress_blocks)
         self.app.title = progress_bar + ' {:2d}:{:02d}'.format(mins, secs)
+
         mins = time_left // 60 if time_left >= 0 else time_left // 60 + 1
         secs = time_left % 60 if time_left >= 0 else (-1 * time_left) % 60
         self.statistics['total_time'] += 1
@@ -140,7 +156,6 @@ class PomodoroApp(object):
             self.stop_button.set_callback(self.stop_timer)
             self.app.title = '{:2d}:{:02d}'.format(mins, secs)
         sender.count += 1
-
 
     def start_break(self, break_type):
         if break_type == "short":
